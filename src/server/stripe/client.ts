d194e0dffb825ae659095c4dd037e7f1f19ac2c7
CloudCodes <85653592+CloudCodesStuff@users.noreply.github.com>
2023-10-08 23:01:38 -0400

    remove

--- src/server/stripe/client.ts
+++ src/server/stripe/client.ts
@@ -2,7 +2,5 @@ import Stripe from "stripe";
 import { env } from "@/env.mjs";
 
 export const stripe = new Stripe(env.STRIPE_SK, {
-  //@ts-ignore
-
-  apiVersion: "2022-11-15",
+  apiVersion: "2023-08-16",
 });
